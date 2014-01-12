12ca22ba7c5933f041cfa14273db616b103c96b6
Mark Baltrusaitis <markbalt@gmail.com>
2014-01-11 23:32:33 -0500

    Added basic add and delete item functionality

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Technology Colophon
 --------------------------------------
 
 - [Symfony 2](http://symfony.com/)
-- [jQUery 1.10.x](http://jquery.com/)
+- [jQuery 1.10.x](http://jquery.com/)
 - HTML5 [with Initializr 4.0](http://www.initializr.com/)
 - [Bootstrap 3.0.3](http://getbootstrap.com/)
 - [MongoDB](http://www.mongodb.org/)
--- src/Telegraf/ChecklistBundle/Controller/DefaultController.php
+++ src/Telegraf/ChecklistBundle/Controller/DefaultController.php
@@ -6,59 +6,77 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Telegraf\ChecklistBundle\Document\Item;
 
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-    	$items = $this->get('doctrine_mongodb')
-	        ->getRepository('TelegrafChecklistBundle:Item')
-	        ->findAll();
+  public function indexAction()
+  {
+  	$items = $this->get('doctrine_mongodb')
+        ->getRepository('TelegrafChecklistBundle:Item')
+        ->findAll();
 
-        return $this->render('TelegrafChecklistBundle:Default:index.html.twig', array('items' => $items));
-    }
+      return $this->render('TelegrafChecklistBundle:Default:index.html.twig', array('items' => $items));
+  }
 
-    public function createItemAction($text)
+  public function createItemAction(Request $request)
 	{
-	    $item = new Item();
-	    $item->setText($text);
-	    
-	    $dm = $this->get('doctrine_mongodb')->getManager();
-	    $dm->persist($item);
-	    $dm->flush();
+  	$text = $request->request->get('text');
 
-	    // return new Response('Created item id '.$item->getText());
-	    return $this->redirect($this->generateUrl('homepage'));
+    $item = new Item();
+    $item->setText($text);
+    
+    $dm = $this->get('doctrine_mongodb')->getManager();
+    $dm->persist($item);
+    $dm->flush();
+
+    // prepare the response
+		$response = array(
+			"id" => $item->getId(),
+			"text" => $item->getText(),
+		);
+
+		// return json
+    return new Response(json_encode($response)); 
 	}
 
 	public function updateItemAction($id)
 	{
-	    $dm = $this->get('doctrine_mongodb')->getManager();
-	    $item = $dm->getRepository('TelegrafChecklistBundle:Item')->find($id);
+    $dm = $this->get('doctrine_mongodb')->getManager();
+    $item = $dm->getRepository('TelegrafChecklistBundle:Item')->find($id);
 
-	    if (!$item) {
-	        throw $this->createNotFoundException('No checklist item found for id '.$id);
-	    }
+    if (!$item) {
+      throw $this->createNotFoundException('No checklist item found for id '.$id);
+    }
 
-	    $item->setName('New product name!');
-	    $dm->flush();
+    $item->setName('New product name!');
+    $dm->flush();
 
-	    return $this->redirect($this->generateUrl('homepage'));
+    return $this->redirect($this->generateUrl('homepage'));
 	}
 
-	public function deleteItemAction($id)
+	public function deleteItemAction(Request $request)
 	{
-	    $dm = $this->get('doctrine_mongodb')->getManager();
-	    $item = $dm->getRepository('TelegrafChecklistBundle:Item')->find($id);
+		$id = $request->request->get('id');
 
-	    if (!$item) {
-	        throw $this->createNotFoundException('No checklist item found for id '.$id);
-	    }
+    $dm = $this->get('doctrine_mongodb')->getManager();
+    $item = $dm->getRepository('TelegrafChecklistBundle:Item')->find($id);
 
-	    $dm->remove($product);
+    if (!$item) {
+        throw $this->createNotFoundException('No checklist item found for id '.$id);
+    }
+
+	  $dm->remove($item);
 		$dm->flush();
 
-	    return $this->redirect($this->generateUrl('homepage'));
+		// prepare the response
+		$response = array(
+			"id" => $item->getId(),
+			"msg" => "Deleted item."
+		);
+
+    return new Response(json_encode($response)); 
 	}
 }
