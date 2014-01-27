d65f2cc15a6eca3129576fe7b2a131c1439cc3b8
Mark Baltrusaitis <markbalt@gmail.com>
2014-01-26 22:15:15 -0500

    Enabled fos user bundle, customized twig templates

--- src/Telegraf/ChecklistBundle/Document/User.php
+++ src/Telegraf/ChecklistBundle/Document/User.php
@@ -2,175 +2,22 @@
 
 namespace Telegraf\ChecklistBundle\Document;
 
-use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
-
-use Symfony\Component\Validator\Constraints as Assert;
-
-use Doctrine\Bundle\MongoDBBundle\Validator\Constraints\Unique as MongoDBUnique;
-
-use Symfony\Component\Security\Core\User\UserInterface;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 
 /**
- * @ODM\Document(collection="users", repositoryClass="Telegraf\ChecklistBundle\Document\UserRepository")
- * @MongoDBUnique(fields="email")
+ * @MongoDB\Document
  */
-class User implements UserInterface, \Serializable
+class User extends BaseUser
 {
     /**
-     * @ODM\Id
+     * @MongoDB\Id(strategy="auto")
      */
     protected $id;
 
-    /**
-     * @ODM\Field(type="string")
-     * @Assert\NotBlank()
-     * @Assert\Email()
-     */
-    protected $email;
-    
-    /**
-     * @ODM\Field(type="string")
-     * @Assert\NotBlank()
-     */
-    protected $username;
-
-    /**
-     * @ODM\Field(type="string")
-     * @Assert\NotBlank()
-     * @Assert\Length(max = 4096)
-     */
-    protected $password;
-    
-    /**
-     * @ODM\Field(name="is_active", type="boolean")
-     */
-    private $isActive;
-
     public function __construct()
     {
-        $this->isActive = true;
-        // may not be needed, see section on salt below
-        // $this->salt = md5(uniqid(null, true));
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getSalt()
-    {
-        // you *may* need a real salt depending on your encoder
-        // see section on salt below
-        return null;
-    }
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    // stupid simple encryption (please don't copy it!)
-    public function setPassword($password)
-    {
-        $this->password = sha1($password);
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    public function getRoles()
-    {
-        return array('ROLE_USER');
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function eraseCredentials()
-    {
-    }
-
-    /**
-     * @see \Serializable::serialize()
-     */
-    public function serialize()
-    {
-        return serialize(array(
-            $this->id,
-            $this->username,
-            $this->password,
-            // see section on salt below
-            // $this->salt,
-        ));
-    }
-
-    /**
-     * @see \Serializable::unserialize()
-     */
-    public function unserialize($serialized)
-    {
-        list (
-            $this->id,
-            $this->username,
-            $this->password,
-            // see section on salt below
-            // $this->salt
-        ) = unserialize($serialized);
-    }
-
-    /**
-     * Set username
-     *
-     * @param string $username
-     * @return self
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-        return $this;
-    }
-
-    /**
-     * Set isActive
-     *
-     * @param boolean $isActive
-     * @return self
-     */
-    public function setIsActive($isActive)
-    {
-        $this->isActive = $isActive;
-        return $this;
-    }
-
-    /**
-     * Get isActive
-     *
-     * @return boolean $isActive
-     */
-    public function getIsActive()
-    {
-        return $this->isActive;
+        parent::__construct();
+        // your own logic
     }
 }
